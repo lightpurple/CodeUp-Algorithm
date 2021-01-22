3196a8660878a67cd3a8418e12507b6e1e2ca007
lightpurple <dmstjd10@gmail.com>
2021-01-22 23:06:36 +0900

    21.01.22

---
+++ 코드업1253.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int a,b;
+	scanf("%d %d", &a, &b);
+
+	if(a > b){
+		int i;
+		for(i=b; i<=a;i++){
+			printf("%d ", i);
+		}
+	}else if(a < b){
+		int i;
+		for(i=a; i<=b;i++){
+			printf("%d ", i);
+		}
+	} 
+	return 0;
+}
